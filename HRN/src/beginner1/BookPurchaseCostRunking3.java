213816cd8698168f4c3162a15293f1e539abf99c
Kohki Ishizuka <k.ishizuka@hr-next.co.jp>
2023-10-20 17:22:07 +0900

    修正版

--- HRN/src/beginner1/BookPurchaseCostRunking3.java
+++ HRN/src/beginner1/BookPurchaseCostRunking3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BookPurchaseCostRunking3 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
 		Scanner sc = new Scanner(System.in);
 		
 		//人数
