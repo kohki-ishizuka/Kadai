17541502b7dcad0d28aab9aa930649fefcb771cf
Kohki Ishizuka <k.ishizuka@hr-next.co.jp>
2023-10-24 10:29:48 +0900

    嫌いな数字

---
+++ HRN/src/begineer2/HateNumber.java
@@ -0,0 +1,56 @@
+package begineer2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class HateNumber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		//①嫌いな数字を入力
+		String HNum = sc.next();
+		int n = Integer.parseInt(HNum);
+		if (n >= 1 && n <= 9) {
+		} else {
+			System.out.println("無効な入力です");
+		}
+
+		//②病室の総数を入力
+		int RNum = sc.nextInt();
+		if (RNum >= 1 && RNum <= 100) {
+		} else {
+			System.out.println("無効な入力です");
+		}
+
+		//③希望する部屋番号
+		List<String> preRoomNum = new ArrayList<>();
+
+		//部屋番号を入力
+		for (int i = 0; i < RNum; i++) {
+			String r = sc.next();
+			int b = Integer.parseInt(r);
+			preRoomNum.add(r);
+			if (b >= 1 && b <= 1000) {
+			} else {
+				System.out.println("無効な入力です");
+			}
+		}
+
+		//嫌いな数字を含む部屋番号を除外
+		List<String> list = preRoomNum.stream() //streamの取得
+				.filter(r -> !r.contains(HNum)) //中間操作
+				.collect(Collectors.toList()); //終端操作
+
+		//結果を出力
+		if (list.isEmpty()) {
+			System.out.println("none");
+		} else {
+			System.out.println(list);
+		}
+		sc.close();
+	}
+}
